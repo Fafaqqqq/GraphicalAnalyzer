a785bd7019d523dfe125be3a7c234aab0a32738e
Fafaqqqq <loda_228@ramble.ru>
2022-01-03 12:33:45 +0300

    add CLI

--- LogConverter/main.cpp
+++ LogConverter/main.cpp
@@ -4,11 +4,39 @@
 
 int main(int argc, char** argv)
 {
-    if (argc == 1 || argc == 2 && (std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h"))
-    {
-        std::cout << "help topic\n";
-    }
+    std::string from;
+    std::string to;
+    std::string filter;
 
-    //LogConverter().Convert(argv[1], argv[3], argv[2]);
+    for (int i = 1; i < argc; i++)
+    {
+        if (std::string(argv[i]) == "--help" || std::string(argv[i]) == "-h")
+        {
+            std::cout << "LogConverter [<log_file>] [-f <filter_file>] [-o <output_file>]\n\n";
+
+            std::cout << "Argument description:\n";
+            std::cout << "\t <log_file>              path to source file with logs\n";
+            std::cout << "\t -f <filter_file>        path to filters file\n";
+            std::cout << "\t -o <output_file>        path to source file for GUI\n";
+            std::cout << "\t -h [--help]             print this message\n";
+
+            exit(0);
+        }
+        else if (std::string(argv[i]) == "-f")
+        {
+            filter = argv[i + 1];
+            i++;
+        }
+        else if (std::string(argv[i]) == "-o")
+        {
+            to = argv[i + 1];
+            i++;
+        }
+        else
+        {
+            from = argv[i];
+        }
+    }
+    LogConverter().Convert(from, to, filter);
     return 0;
 }
